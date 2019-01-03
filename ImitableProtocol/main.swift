6604a9121ce91a7918078758979f5e69e46bf9fe
mohammed-7418 <imthathullah.m@zohocorp.com>
2019-01-03 15:43:11 +0530

    Initial Commit

---
+++ ImitableProtocol/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ImitableProtocol
+//
+//  Created by Imthath M on 03/01/19.
+//  Copyright © 2019 Zoho Corp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
